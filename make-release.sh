0b311bcbb59a15275cce7672ba60bcc45094efd0
Steffen A. Mork <linux-dev@morknet.de>
2016-02-17 09:39:03 +0000

    * Release- und Snapshot Bump-Skripte ergänzt.
    git-svn-id: svn+ssh://svn.code.sf.net/p/sctqtgen/code/trunk@23 3dc4218d-7d7a-4c0d-b2d6-54770c2ab8e5

---
+++ make-release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=1.0.0
+
+for FILE in de.morknet.sct.qt.*/pom.xml
+do
+	echo $FILE
+	sed -e "0,/${VERSION}-SNAPSHOT/s//${VERSION}/" -i ${FILE}
+done
+
+for FILE in de.morknet.sct.qt.*/feature.xml de.morknet.sct.qt.*/META-INF/MANIFEST.MF
+do
+	echo $FILE
+	sed -e "s/${VERSION}.qualifier/${VERSION}/g" -i ${FILE}
+done
+
+sed -e "s/%RELEASE%/true/g" -e "s/%SNAPSHOT%/false/g" p2.template >de.morknet.sct.qt.feature/p2.inf
---
+++ make-snapshot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION_OLD=1.0.0
+VERSION_NEW=1.0.1
+
+for FILE in de.morknet.sct.qt.*/pom.xml
+do
+	echo $FILE
+	sed -e "0,/${VERSION_OLD}/s//${VERSION_NEW}-SNAPSHOT/" -i ${FILE}
+done
+
+for FILE in de.morknet.sct.qt.*/feature.xml de.morknet.sct.qt.*/META-INF/MANIFEST.MF
+do
+	echo $FILE
+	sed -e "s/${VERSION_OLD}/${VERSION_NEW}.qualifier/g" -i ${FILE}
+done
+
+for FILE in de.morknet.sct.qt.updatesite/*.xml
+do
+	echo $FILE
+	sed -e "s/${VERSION_OLD}.qualifier/${VERSION_NEW}.qualifier/g" -i ${FILE}
+done
+
+sed -e "s/%RELEASE%/false/g" -e "s/%SNAPSHOT%/true/g" p2.template >de.morknet.sct.qt.feature/p2.inf
