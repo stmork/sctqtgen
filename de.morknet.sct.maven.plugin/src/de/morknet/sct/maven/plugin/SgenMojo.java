a53aaf49182eb2a12eee3f6885260389aff1ec1a
Steffen A. Mork <linux-dev@morknet.de>
2016-02-17 09:21:27 +0000

    * Dateien ergänzt.  * sgen-Mojo angelegt.
    git-svn-id: svn+ssh://svn.code.sf.net/p/sctqtgen/code/trunk@21 3dc4218d-7d7a-4c0d-b2d6-54770c2ab8e5

---
+++ de.morknet.sct.maven.plugin/src/de/morknet/sct/maven/plugin/SgenMojo.java
@@ -0,0 +1,27 @@
+package de.morknet.sct.maven.plugin;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "sct", defaultPhase = LifecyclePhase.TEST)
+public class SgenMojo extends AbstractMojo
+{
+
+	@Parameter(name="sgen", required=true)
+	private File sgen;
+
+    /**
+	 * Says "Hi" to the user.
+	 *
+	 */
+	public void execute() throws MojoExecutionException
+	{
+		getLog().info("Hello, world.");
+		getLog().info(sgen.getAbsolutePath());
+	}
+}
