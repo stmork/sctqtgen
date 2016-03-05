2db542101bb69b1686e660ddeec6b568ad99aede
Steffen A. Mork <linux-dev@morknet.de>
2016-03-05 10:11:59 +0000

    * Version bump.
    git-svn-id: svn+ssh://svn.code.sf.net/p/sctqtgen/code/trunk@60 3dc4218d-7d7a-4c0d-b2d6-54770c2ab8e5

--- make-release.sh
+++ make-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=1.0.2
+VERSION=1.0.3
 
 for FILE in de.morknet.sct.qt.*/pom.xml
 do
--- make-snapshot.sh
+++ make-snapshot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION_OLD=1.0.2
-VERSION_NEW=1.0.3
+VERSION_OLD=1.0.3
+VERSION_NEW=1.0.4
 
 for FILE in de.morknet.sct.qt.*/pom.xml
 do
