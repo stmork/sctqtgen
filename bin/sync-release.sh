09991bce70a49dbaeaa97d7a72d32c4e8f71d395
Steffen A. Mork <linux-dev@morknet.de>
2016-08-09 17:40:56 +0000

    * Repository has moved to a new location.
    git-svn-id: svn+ssh://svn.code.sf.net/p/sctqtgen/code/trunk@101 3dc4218d-7d7a-4c0d-b2d6-54770c2ab8e5

--- bin/sync-release.sh
+++ bin/sync-release.sh
@@ -2,3 +2,5 @@
 
 rsync -av --delete de.morknet.sct.qt.updatesite/target/repository/\
 	stmork,sctqtgen@web.sourceforge.net:/home/pfs/project/sctqtgen/repositories/release/
+rsync -av --delete de.morknet.sct.qt.updatesite/target/repository/\
+	stmork,sctqtgen@web.sourceforge.net:/home/project-web/sctqtgen/htdocs/updates/release/
--- bin/sync-snapshot.sh
+++ bin/sync-snapshot.sh
@@ -2,3 +2,5 @@
 
 rsync -av --delete de.morknet.sct.qt.updatesite/target/repository/\
 	stmork,sctqtgen@web.sourceforge.net:/home/pfs/project/sctqtgen/repositories/snapshot/
+rsync -av --delete de.morknet.sct.qt.updatesite/target/repository/\
+	stmork,sctqtgen@web.sourceforge.net:/home/project-web/sctqtgen/htdocs/updates/snapshot/
