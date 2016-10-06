aafa149c542927d78749cc282d95aecbb3290732
Steffen A. Mork <linux-dev@morknet.de>
2016-10-06 17:22:13 +0000

    * Added code generator for headless build.
    git-svn-id: svn+ssh://svn.code.sf.net/p/sctqtgen/code/trunk@110 3dc4218d-7d7a-4c0d-b2d6-54770c2ab8e5

--- de.morknet.sct.qt.generator/src/de/morknet/sct/qt/generator/QtCodeGenerator.java
+++ de.morknet.sct.qt.generator/src/de/morknet/sct/qt/generator/QtCodeGenerator.java
@@ -7,17 +7,11 @@
 package de.morknet.sct.qt.generator;
 
 import org.yakindu.sct.generator.core.impl.GenericJavaBasedGenerator;
-import org.yakindu.sct.generator.core.types.ICodegenTypeSystemAccess;
 import org.yakindu.sct.model.sexec.ExecutionFlow;
-import org.yakindu.sct.model.sexec.naming.INamingService;
 import org.yakindu.sct.model.sgen.GeneratorEntry;
 import org.yakindu.sct.model.sgraph.Statechart;
 
-//import com.google.inject.Binder;
 import com.google.inject.Inject;
-//import com.google.inject.Module;
-//import com.google.inject.name.Names;
-//import com.google.inject.util.Modules;
 
 public class QtCodeGenerator extends GenericJavaBasedGenerator
 {
@@ -27,7 +21,7 @@ public class QtCodeGenerator extends GenericJavaBasedGenerator
 	@Override
 	public void runGenerator(Statechart statechart, GeneratorEntry entry)
 	{
-		ExecutionFlow flow = createExecutionFlow(statechart, entry);
+		final ExecutionFlow flow = createExecutionFlow(statechart, entry);
 
 		if (debugFeatureHelper.isDumpSexec(entry))
 		{
